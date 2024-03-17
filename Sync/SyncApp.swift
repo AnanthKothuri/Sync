b464fcef982cd37bd65098c2aee4627fd45c9f16
Ananth Kothuri <ananth.s.kothuri@gmail.com>
2024-03-17 11:05:29 -0500

    Initial Commit

---
+++ Sync/SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncApp.swift
+//  Sync
+//
+//  Created by Ananth Kothuri on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
